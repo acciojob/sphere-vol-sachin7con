18ccea01fae4abd8004e826cf67df1f5b99dc616
sachin7con <86023048+sachin7con@users.noreply.github.com>
2024-03-14 12:18:55 +0530

    created a submission

--- script.js
+++ script.js
@@ -1,6 +1,9 @@
 function volume_sphere() {
-    //Write your code here
-  
+    //SGN Write your code here
+	 event.preventDefault();
+	var rad = parseFloat(document.getElementById("radius").value);
+	var vol = (4/3) * Math.PI * Math.pow(rad, 3);
+	document.getElementById("volume").value = vol.toFixed(4);
 } 
 
 window.onload = document.getElementById('MyForm').onsubmit = volume_sphere;
